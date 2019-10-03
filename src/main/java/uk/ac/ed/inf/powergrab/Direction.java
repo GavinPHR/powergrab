37d3a436da4ed49216ae555c4f5a5d75f3516eb1
Gavin Peng <gavinsweden@gmail.com>
2019-10-03 12:52:01 +0100

    0310/2

--- src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,9 +1,9 @@
 package uk.ac.ed.inf.powergrab;
 
 // Enum class with all 16 compass directions
-// For each direction, horizontal (longitudinal) and 
-// vertical (latitudinal) change for a net change of 0.0003 unit
-// are pre-computed 
+// For each direction, horizontal (longitudinal) change and 
+// vertical (latitudinal) change are pre-computed.
+// The net change is 0.0003 unit.
 public enum Direction {
     E   (3.0E-4,                 0.0),
     ENE (2.7716385975338597E-4,  1.1480502970952692E-4),
@@ -30,11 +30,12 @@ public enum Direction {
         vertical = v;
     }
     
-    double getHorizontal() {
+    // Public getters that return the changes
+    public double getHorizontal() {
         return horizontal;
     }
     
-    double getVertical() {
+    public double getVertical() {
         return vertical;
     }
 }
--- src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -3,6 +3,8 @@ package uk.ac.ed.inf.powergrab;
 public class Position {
     public double latitude;
     public double longitude;
+  
+    // The edges of play area 
     private final double top = 55.946233;
     private final double bottom = 55.942617;
     private final double left = -3.192473;
@@ -23,9 +25,4 @@ public class Position {
         return latitude > bottom && latitude < top && 
                longitude < right && longitude > left;
     }
-    
-//    public static void main(String[] args) {
-//        Position p = new Position(55.944425,-3.188396);
-//        System.out.println(p.nextPosition(null).latitude + "  " + p.nextPosition(Direction.SSE).longitude);
-//    }
 }
